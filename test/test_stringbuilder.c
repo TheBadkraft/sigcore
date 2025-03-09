48e811e390cb26abf57165dd22554d572e962002
badkraft <thebadkraft@gmail.com>
2025-03-09 06:55:13 -0500

    string_buildeer enhancements
    fixed append-line bug (appendl)
    added line-append-string (lappends)
    added line-append-format (lappendf)

--- test/test_stringbuilder.c
+++ test/test_stringbuilder.c
@@ -73,6 +73,88 @@ void appendf_sb(void) {
 	Mem.free(actResult);
 	StringBuilder.free(sb);
 }
+/* new sb from string (char* buffer) */
+void snew_sb(void) {
+	string str = "A char* buffer";
+	int expLength = strlen(str);
+	
+	printf("\nbase string '%s' length=%d\n", str, expLength);
+	
+	string_builder sb = StringBuilder.snew(str);
+	size_t actLength = StringBuilder.length(sb);
+	
+	Assert.isTrue(expLength == actLength, "length mismatch");
+	printf("sb string   '%s' length=%ld  : ", StringBuilder.toString(sb), actLength);
+	
+	StringBuilder.free(sb);
+}
+/* append line */
+void appendl_sb(void) {
+	string str = "A char* buffer";
+	string appStr = "with appended line";
+	
+	int expLength = snprintf(NULL, 0, "%s%s\n", str, appStr);
+	printf("\noutput length=%d\n", expLength);
+	
+	string_builder sb = StringBuilder.snew(str);
+	StringBuilder.appendl(sb, appStr);
+	size_t actLength = StringBuilder.length(sb);
+	
+	string expOutput = "A char* bufferwith appended line\n";
+	string actOutput = StringBuilder.toString(sb);
+	
+	Assert.isTrue(expLength == actLength, "length mismatch");
+	Assert.isTrue(strcmp(expOutput, actOutput) == 0, "string mismatch");
+	printf(" (output)\n'%s' length=%ld  : ", actOutput, actLength);
+	
+	Mem.free(actOutput);
+	StringBuilder.free(sb);
+}
+/* append line + string */
+void lappends_sb(void) {
+	string str = "A char* buffer";
+	string appStr = "with appended line";
+	
+	int expLength = snprintf(NULL, 0, "%s\n%s", str, appStr);
+	printf("\noutput length=%d\n", expLength);
+	
+	string_builder sb = StringBuilder.snew(str);
+	StringBuilder.lappends(sb, appStr);
+	size_t actLength = StringBuilder.length(sb);
+	
+	string expOutput = "A char* buffer\nwith appended line";
+	string actOutput = StringBuilder.toString(sb);
+	
+	Assert.isTrue(expLength == actLength, "length mismatch");
+	Assert.isTrue(strcmp(expOutput, actOutput) == 0, "string mismatch");
+	printf(" (output)\n'%s' length=%ld  : ", actOutput, actLength);
+	
+	Mem.free(actOutput);
+	StringBuilder.free(sb);
+}
+/* append line + formatted_string */
+void lappendf_sb(void) {
+	string str = "A char* buffer";
+	string appStr = "with appended line";
+	string fmtStr = "(formatted)";
+	
+	int expLength = snprintf(NULL, 0, "%s\n%s %s", str, appStr, fmtStr);
+	printf("\noutput length=%d\n", expLength);
+	
+	string_builder sb = StringBuilder.snew(str);
+	StringBuilder.lappendf(sb, "%s %s", appStr, fmtStr);
+	size_t actLength = StringBuilder.length(sb);
+	
+	string expOutput = "A char* buffer\nwith appended line (formatted)";
+	string actOutput = StringBuilder.toString(sb);
+	
+	Assert.isTrue(expLength == actLength, "length mismatch");
+	Assert.isTrue(strcmp(expOutput, actOutput) == 0, "string mismatch");
+	printf(" (output)\n'%s' length=%ld  : ", actOutput, actLength);
+	
+	Mem.free(actOutput);
+	StringBuilder.free(sb);
+}
 
 // Register test cases
 __attribute__((constructor)) void init_sigtest_tests(void) {
@@ -80,4 +162,8 @@ __attribute__((constructor)) void init_sigtest_tests(void) {
 	register_test("sb_to_string", sb_to_string);
 	register_test("append_sb", append_sb);
 	register_test("appendf_sb", appendf_sb);
+	register_test("snew_sb", snew_sb);
+	register_test("appendl_sb", appendl_sb);
+	register_test("lappends_sb", lappends_sb);
+	register_test("lappendf_sb", lappendf_sb);
 }
